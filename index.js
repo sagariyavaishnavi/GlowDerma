f28050f730da84a5c58318e7cbbf566b5a00687a
sagariyavaishnavi <sagariyavaishnavi@gmail.com>
2025-01-21 17:57:37 +0530

    About SkinCare website

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from "express";
 const app = express();
-
 let PORT = process.env.PORT || 5000;
-
 
 app.get("/", (req, res) => {
     res.send("Welcome to GlowDerma - Your Skincare Journey Begins Here.");
